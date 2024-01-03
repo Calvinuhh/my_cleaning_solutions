1f0adba123a57acc3f59a94515504e9c27997e38
Calvin Ulis Howard Howard <Calvin.uhh@gmail.com>
2024-01-03 18:10:20 -0500

    Agregando imagen de fondo y añadiendo estilos al nav-bar

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
+app.use(cors());
+app.use(express.static(path.join(__dirname)));
 
 app.get("/", (req, res) => {
   res.sendFile("/src/index.html", {
@@ -12,8 +14,30 @@ app.get("/", (req, res) => {
   });
 });
 
-app.use(cors());
-app.use(express.static(path.join(__dirname)));
+app.get("/index.html", (req, res) => {
+  res.sendFile("/src/index.html", {
+    root: __dirname,
+  });
+});
+
+app.get("/nosotros.html", (req, res) => {
+  res.sendFile("/src/nosotros.html", {
+    root: __dirname,
+  });
+});
+
+app.get("/servicios.html", (req, res) => {
+  res.sendFile("/src/servicios.html", {
+    root: __dirname,
+  });
+});
+
+app.get("/contactanos.html", (req, res) => {
+  res.sendFile("/src/contactanos.html", {
+    root: __dirname,
+  });
+});
+
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en puerto: http://localhost:${PORT}
